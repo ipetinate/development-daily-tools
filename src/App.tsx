d36ea0218d0a4180de80839184f3b74e1414ac43
Isac Petinate <idpetinate@gmail.com>
2023-03-27 13:27:48 -0300

    Implementing home

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react'
+import logo from './assets/tools.png'
+import { DownloadButton } from './components/DownloadButton'
+import { ExampleTable } from './components/ExampleTable'
+import { ProgressBar } from './components/ProgressBar'
+import { useFileTools } from './hooks/useFileTools'
+
+export function App() {
+    const {
+        handleChangeFileInput,
+        convertTxtToJson,
+        resetProgress,
+        handleDowloadFile,
+        fileContent,
+        processingStatus
+    } = useFileTools()
+
+    return (
+        <div className='flex flex-col gap-10 items-center justify-center content-center h-full w-full '>
+            <div className='flex flex-col items-center gap-5 p-5'>
+                <img src={logo} alt='App logo' className='w-40 h-40' />
+
+                <p className='text-xl'>
+                    Olá, bem-vindo, converta arquivos TXT estruturado para JSON
+                </p>
+            </div>
+
+            <div className='flex flex-col gap-5'>
+                <ExampleTable />
+
+                <div className='flex flex-col gap-10 p-10 border border-violet-900 rounded-md'>
+                    <p className=''>Selecione seu arquivo .txt</p>
+
+                    <input
+                        type='file'
+                        name='file'
+                        id='file'
+                        accept='.txt'
+                        onClick={resetProgress}
+                        onChange={handleChangeFileInput}
+                    />
+                </div>
+
+                <div className='flex flex-col gap-10 p-10 border border-violet-900 rounded-md'>
+                    <div className='flex flex-col gap-5 w-full'>
+                        <h3>Enviando arquivo ({processingStatus + '%'})</h3>
+
+                        <ProgressBar progress={processingStatus} />
+                    </div>
+
+                    <div className='flex flex-row items-center justify-center'>
+                        <DownloadButton
+                            disabled={!processingStatus}
+                            handleDownload={() =>
+                                handleDowloadFile(convertTxtToJson(fileContent))
+                            }
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
